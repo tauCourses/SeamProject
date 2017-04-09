63911b60f5d42133097d2ec7f4a178371ea0842c
itay itzhak <LenovoPc@Itayit.(none)>
2017-04-09 18:58:10 +0300

    savePictureAdded

--- Image.java
+++ Image.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,5 +22,33 @@ public class Image {
 		}
 	}
 	
+	public int getSizeX()
+	{
+		return this.sizeX;
+	}
+	
+	public int getSizeY()
+	{
+		return this.sizeY;
+	}
+	
+	public Pixel[][] getPixels()
+	{
+		return this.pixels;
+	}
+	
+	public void saveBufferedImageFromImage(String path)
+	{
+		BufferedImage bufferedImage = new BufferedImage(this.sizeX , this.sizeY, BufferedImage.TYPE_INT_ARGB);
+		for (int i = 0; i < sizeX; i++)
+		{
+			for (int j = 0 ; j < sizeY; j++)
+			{
+				bufferedImage.setRGB(i, j, (new Color(this.pixels[i][j].red, this.pixels[i][j].green, this.pixels[i][j].blue)).getRGB());
+			}
+		}
+		File outputfile = new File(path);
+		ImageIO.write(bufferedImage, "jpg", outputfile);
+	}
 			
 }

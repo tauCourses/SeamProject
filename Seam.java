cbe3c959f9bb335dfd898d1d9918f7ac2e21c7c3
Matan laptop <matan.harel.mh@gmail.com>
2017-04-09 17:24:01 +0300

    init

---
+++ Seam.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Seam {
+	
+	public static void main(String[] args)
+	{
+		 try
+	    {
+	      // the line that reads the image file
+	      BufferedImage image = ImageIO.read(new File("/home/matan/Pictures/nepalFBpics/CIMG2836.JPG"));
+	      System.out.println(image.getHeight());
+	      // work with the image here ...
+	    } 
+	    catch (IOException e)
+	    {
+	      // log the exception
+	      // re-throw if desired
+	    }
+	}
+}
